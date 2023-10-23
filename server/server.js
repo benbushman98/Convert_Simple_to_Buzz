16a95b2d0338906ceb237bd32109e29e15385fba
Benjamin Bushman <benjamin@HelmsDeep.hsd1.ut.comcast.net>
2023-10-23 11:14:40 -0600

    added new path

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.get('/api/download/:fileName', (req, res) => {
 });
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, 'uploads/'); // Define the destination folder for uploaded files
+        callback(null, './uploads'); // Define the destination folder for uploaded files
     },
     filename: (req, file, callback) => {
         callback(null, file.originalname); // Use the original filename for the uploaded file
